be9d09ca1f4e1707e7d3b1b45f872538be84ca54
Owen Beste <beste_owen@bah.com>
2018-08-02 00:31:25 -0400

    function expressions

--- app.js
+++ app.js
@@ -14,3 +14,12 @@ function logGreeting(fn) {
 // this shows passing a function to another function as
 // a first-class object.
 logGreeting(greet);
+
+// function expression
+// this is a var that contains an anonymous function
+// you can still pass this to other functions
+var greetMe = function() {
+    console.log( 'hello world' );
+}
+
+logGreeting(greetMe);
