4c513436f62a2db64f2ef189564be31c4905679e
Owen Beste <beste_owen@bah.com>
2018-08-05 21:43:17 -0400

    start require

--- app.js
+++ app.js
@@ -1,25 +1,3 @@
-// function statement
-function greet() {
-    console.log('hi');
-}
+require('./greet.js');
 
-//greet();
 
-function logGreeting(fn) {
-    fn();
-}
-
-// NO Parens because putting () would *run* the function
-// instead we just want to *pass* the function itself
-// this shows passing a function to another function as
-// a first-class object.
-logGreeting(greet);
-
-// function expression
-// this is a var that contains an anonymous function
-// you can still pass this to other functions
-var greetMe = function() {
-    console.log( 'hello world' );
-}
-
-logGreeting(greetMe);
