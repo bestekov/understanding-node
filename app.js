b07701e89a74c51e4a1d37c0558516e5938655dd
Owen Beste <beste_owen@bah.com>
2018-08-05 21:56:58 -0400

    Examples of object literals

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-require('./greet.js');
+var person = {
+    firstname: 'John',
+    lastname: 'Doe',
+    greet: function() {
+        console.log(`Hello ${this.firstname} ${this.lastname}`);
+    }
+}
 
+// you can access properties with a name
+person.greet();
 
+// You can name object properties without a dot
+console.log(person[ 'firstname' ]);
