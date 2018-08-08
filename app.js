81219c54cf8d76982bc62f1d998b217cbaa79003
Owen Beste <beste_owen@bah.com>
2018-08-08 00:40:00 -0400

    Event emitter pt3 - switch to ES6 syntax

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-// Inheriting from event emitter
+// Inheriting from event emitter pt 3 - use ES6
+'use strict';
 
 var EventEmitter = require('events');
-var util = require('util');
 
-function Greetr() {
-    this.greeting = 'Hello world!';
+
+// The ES6 syntax is extends instead of needing utils inherits
+// This is just syntactic sugar that makes it easier to type
+// but under the hood it is working exactly how it worked before
+class Greetr extends EventEmitter{
+    constructor() {
+        super();
+        this.greeting = 'Hello world!';
+    }
+
+    greet( data ) {
+        console.log(this.greeting + ': ' + data );
+        // It can do this.emit because it inherited from EvenEmitter
+        this.emit( 'greet', data );
+    }
 }
-
-// anything that inherits from Greetr should also get the prototypes from 
-/// EventEmitter. So we can use the on and emit methods!
-util.inherits(Greetr, EventEmitter);
-
-Greetr.prototype.greet = function( data ) {
-    console.log(this.greeting + ': ' + data );
-    // It can do this.emit because it inherited from EvenEmitter
-    this.emit( 'greet', data );
-}
-
 
 var greeter1 = new Greetr();
 
