d16770dd3a9b970f5dbf0517e5332cb8b318783d
Owen Beste <beste_owen@bah.com>
2018-08-09 23:41:42 -0400

    Working with streams and files

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-// Working with files
+// Working with streams from files
 
 var fs = require('fs');
 
+// the 'highWaterMark' option allows you to set the buffer smaller
+// that the OS default. the small buffer will fire several chunks
+var readable = fs.createReadStream(
+    './greet.txt', 
+    { encoding:'utf8', highWaterMark: 2*1024 });
 
-// readFileSync means *synchronous* which you would only want
-// if you were doing something like reading a requird config
-// otherwise you might block your execution
-var greet = fs.readFileSync(
-    //__dirname + '/greet.txt', //can use __dirname special var or ./
-    './greet.txt',
-    'utf8',);
 
-console.log(greet);
+var writeable = fs.createWriteStream(
+    './greetcopy.txt'
+);
 
-// instead we can use readFile, that takes a *callback*
-var greet2 = fs.readFile('./greet.txt','utf8',
-    function(err, data) {
-        console.log(data);
+
+// As the text goes into the buffer, if the full contents fits
+// you will get all the data. But if it is bigger than the buffer
+// every time it fills the buffer, it will emit a 'data' event
+// a "chunk" is a set of data from the buffer
+readable.on('data', function(chunk) {
+    //console.log(chunk)
+    console.log(chunk);
+
+    // by having a callback that writes to the file, we can stream the contents
+    // to the writable file with each chunk.
+    writeable.write(chunk);
 });
 
-// having this *after* the call proves that the greet2 function was
-// async, because done fires first!
-console.log('Done!');
