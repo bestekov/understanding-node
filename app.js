6ce1f12a6c987afb7cf09bb17d8d2d863cb3f2df
Owen Beste <beste_owen@bah.com>
2018-08-10 20:54:47 -0400

    Web server - using files and templates

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-// Starting a hello world webserver
+// Web server - serving a separate file
 
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function(req, res) {
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
     
+    // include the separate html file
+    var html = fs.readFileSync( __dirname + '/index.htm',
+        'utf8'); // setting this to utf8 forces the stream to return a string
+    
+    // this messsage allows you to do find and replace on the template string
+    var message = 'Some awesome message text!';
+    html = html.replace('{Message}', message);
+
     // .end is a simple way to send a response to the browser
-    res.end('<h1>Hello world!</h1>\n'
-                + '<p>This is listening to the world!</p>\n');
+    res.end(html);
 
     // .listen specifies the port and IP
 }).listen(1337, '127.0.0.1');
