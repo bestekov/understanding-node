55d7078870316ed6889ce7bda858452e684ef555
Owen Beste <beste_owen@bah.com>
2018-08-10 20:59:44 -0400

    Web server - serving pages as streams

--- app.js
+++ app.js
@@ -7,18 +7,9 @@ http.createServer(function(req, res) {
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
     
-    // include the separate html file
-    var html = fs.readFileSync( __dirname + '/index.htm',
-        'utf8'); // setting this to utf8 forces the stream to return a string
-    
-    // this messsage allows you to do find and replace on the template string
-    var message = 'Some awesome message text!';
-    html = html.replace('{Message}', message);
+    // instead of readFile, we can create a readable stream and pipe to the res
+    fs.createReadStream( __dirname + '/index.htm','utf8').pipe(res);
 
-    // .end is a simple way to send a response to the browser
-    res.end(html);
-
-    // .listen specifies the port and IP
 }).listen(1337, '127.0.0.1');
 
 // Then all you have to do is visit localhost:1337 and that creates an event
