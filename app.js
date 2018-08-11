3946f7863f0e081019f6e000625a9abe9e017f3a
Owen <owen.beste@gmail.com>
2018-08-10 23:09:37 -0400

    NPM, packages, dev dependencies

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-// NPM and packages
+// NPM and packages - dev dependencies and nodemon
 
 var moment = require('moment');
 
+// just a simple command to show that the import worked
 console.log(moment().format("ddd, hA"));
+
+console.log('Hi I updated');
