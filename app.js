850929b7644d80a531dfaa0050ad24f7cff319ae
Owen <owen.beste@gmail.com>
2018-08-11 00:20:51 -0400

    Using Express - Routing, Static Files, and Middleware

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
-// Using Express
+// Using Express - Routing, static files, and middleware
 
 var express = require('express');
 
-// Express is using the same http library we did earlier
-// Instead it just wraps all that other code so that the application
-// code can be way more succinct
 var app = express();
 
-// set equal to the environment variable or 3000
 var port = process.env.PORT || 3000;
 
-// a more powerful version of the server. the request and response
-// are express requests and responses that provide additionaly features
-// additionally express supports standard verbs
+// the .use statement means apply some middleware
+// this middleware ways anytime "assets" appears, serve the static file
+app.use('/assets', express.static(__dirname + '/public'));
+
+// you can add custom middleware. It adds a "next" param
+app.use('/', function(req, res, next) {
+    console.log('Request URL: '+ req.url);
+    next(); // means continue to the next middleware, wich in this case
+            // will be the `get` methods.
+})
+
+
+// By referencing the assets, the css is loaded and the font gets loaded
 app.get('/', function(req, res) {
-    res.send('<html><head></head><body><h1>Hello World!!</h1></body></html>');
+    res.send(`<html><head><link href=assets/style.css 
+    type=text/css rel=StyleSheet 
+    /></head><body><h1>Hello World!!</h1></body></html>`);
+
+});
+
+app.get('/person/:id', function(req, res) {
+    res.send(`<html><head></head><body><h1>Hello World, `
+        + `${req.params.id}!</h1></body></html>`);
 });
 
 app.get('/api', function(req, res) {
     res.json({ firstname: 'Jon', lastname: 'Snow'});
 });
 
-// Sets up the server listener
 app.listen(port);
