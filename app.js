5cedc1d3a4cac63c721ec7808c03405adabf6427
Owen <owen.beste@gmail.com>
2018-08-10 23:33:06 -0400

    Using Express - Converting the basic site to use exprss

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-// NPM and packages - dev dependencies and nodemon
+// Using Express
 
-var moment = require('moment');
+var express = require('express');
 
-// just a simple command to show that the import worked
-console.log(moment().format("ddd, hA"));
+// Express is using the same http library we did earlier
+// Instead it just wraps all that other code so that the application
+// code can be way more succinct
+var app = express();
 
-console.log('Hi I updated');
+// set equal to the environment variable or 3000
+var port = process.env.PORT || 3000;
+
+// a more powerful version of the server. the request and response
+// are express requests and responses that provide additionaly features
+// additionally express supports standard verbs
+app.get('/', function(req, res) {
+    res.send('<html><head></head><body><h1>Hello World!!</h1></body></html>');
+});
+
+app.get('/api', function(req, res) {
+    res.json({ firstname: 'Jon', lastname: 'Snow'});
+});
+
+// Sets up the server listener
+app.listen(port);
